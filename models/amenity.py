e39a18cceb9229ebb8bb20fa80d570e1253e5bf3
magonazz1 <martinmagonazz@outlook.com>
2024-01-23 01:23:49 -0800

    tasks

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the amenity class"""
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+        __tablename__: amenities table for database
+    """
+
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
