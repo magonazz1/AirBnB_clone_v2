8876732520ab3c5a3b76836c8667aeeb873726d4
magonazz1 <martinmagonazz@outlook.com>
2024-01-23 01:32:46 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
+    """Represents an Amenity for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+
     Attributes:
-        name: input name
-        __tablename__: amenities table for database
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
-
-    __tablename__ = 'amenities'
-
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
